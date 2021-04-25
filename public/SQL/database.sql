0d2adae5192dd882c5076fe9801f63027c8d2875
IwanJones41299 <iwanjones41299@gmail.com>
2021-04-25 16:03:58 +0100

    product display

--- public/SQL/database.sql
+++ public/SQL/database.sql
@@ -7,3 +7,19 @@ CREATE TABLE categories(
 
 INSERT INTO categories(cat_title) VALUES ("example 1");
 INSERT INTO categories(cat_title) VALUES ("example 2");
+
+CREATE TABLE products(
+    product_id INT IDENTITY PRIMARY KEY,
+    product_title VARCHAR(255),
+    product_category_id INT,
+    product_price FLOAT,
+    product_description VARCHAR(255),
+    product_image VARCHAR(255)
+)
+
+INSERT INTO products(product_title, product_category_id, product_price, product_description, product_image) VALUES ("product 1", "1", 24.99, "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.", "http://placehold.it/320x150");
+INSERT INTO products(product_title, product_category_id, product_price, product_description, product_image) VALUES ("product 2", "2", 29.99, "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.", "http://placehold.it/320x150");
+
+INSERT INTO products(product_title, product_category_id, product_price, product_description, product_image) VALUES ("product 3", "3", 14.99, "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.", "http://placehold.it/320x150");
+
+INSERT INTO products(product_title, product_category_id, product_price, product_description, product_image) VALUES ("product 4", "4", 17.99, "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.", "http://placehold.it/320x150");
--- resources/functions.php
+++ resources/functions.php
@@ -1,5 +1,6 @@
 <?php
 
+//helper functions
 //redirect page function
 function redirect($location){
     header("Location: $location");
@@ -30,4 +31,28 @@ function fetch_array($result){
     return mysqli_fetch_array($result);
 }
 
-?>
+// get products
+function get_products() {
+$query = query("SELECT * FROM products");
+confirm($query);
+while($row = fetch_array($query)){
+
+$product = <<<DELIMETER
+
+<div class="col-sm-4 col-lg-4 col-md-4">
+    <div class="thumbnail">
+        <img src="http://placehold.it/320x150" alt="">
+        <div class="caption">
+            <h4 class="pull-right">£{$row['product_price']}</h4>
+            <h4><a href="product.html">First Product</a>
+            </h4>
+            <p>See more snippets like this online store item at <a target="_blank" href="http://www.bootsnipp.com">Bootsnipp - http://bootsnipp.com</a>.</p>
+            <a class="btn btn-primary" href="#">Buy Now</a>
+        </div>
+    </div>
+</div>
+DELIMETER;
+
+echo $product;
+    }
+}
